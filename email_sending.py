2d53cc6873c98d4c72666ae9a8eccc3b0c99fcb8
Leo-sudo-SG <63265874+Leo-sudo-SG@users.noreply.github.com>
2024-07-11 16:48:37 +0200

    Add files via upload

--- email_sending.py
+++ email_sending.py
@@ -4,12 +4,12 @@ from email.mime.text import MIMEText
 
 
 
-def send_mail(msg, email_recipient):
+def send_mail(msg,mail_sender,pw_sender, email_recipient):
     s = smtplib.SMTP('smtp.web.de', 587)
     # start TLS for security
     s.starttls()
     # Authentication
-    s.login("fragenfabrik@web.de", "fragenfabrik2024")
+    s.login(mail_sender, pw_sender)
     msg= MIMEText(msg)
     msg['Subject'] = 'Your Product Feedback'
     msg['From'] = 'fragenfabrik@web.de'
