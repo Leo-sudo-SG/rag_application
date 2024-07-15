b6e325452286f490e29a037b14d8badd964e9021
Leo-sudo-SG <63265874+Leo-sudo-SG@users.noreply.github.com>
2024-07-15 10:12:37 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -115,7 +115,8 @@ def interview():
 
     model = ChatOpenAI(model_name="gpt-4o", temperature=0, streaming=True)
     contextualize_q_system_prompt="""Given a chat history and the latest user response
-    which might reference context in the chat history, formulate a possible follow-up question that can be understood without the chat history."""
+    which might reference context in the chat history, formulate a possible follow-up question that can be understood without the chat history.
+    Formulate this question allways in German."""
     contextualize_q_prompt = ChatPromptTemplate.from_messages(
         [
             ("system", contextualize_q_system_prompt),
